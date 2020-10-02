2dbc31a8d8878830eddfb90ed9b34b3cdf4ae117
reymon359 <ramonmorcillocascales@gmail.com>
2020-10-02 19:15:04 +0100

    feat: add eslint standard config

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
