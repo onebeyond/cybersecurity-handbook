1861f78c1653ae38c43b2c4c90462d01e6263a03
reymon359 <ramonmorcillocascales@gmail.com>
2020-10-02 19:27:29 +0100

    refactor: remove react config from linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,24 +5,12 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:react/recommended',
     'standard'
   ],
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
     ecmaVersion: 12,
     sourceType: 'module'
   },
-  plugins: [
-    'react'
-  ],
   rules: {
   }
 }
